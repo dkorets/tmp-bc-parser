38609b8d80597f6553e1b313e03650b5e893e25c
ingenuity <dimakorets@dimas-MacBook-Pro.local>
2024-03-30 20:59:45 +0100

    add parser logic; cronjob

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,6 @@
 
 namespace App\Providers;
 
-use App\Services\Processor;
-use App\Services\ProcessorCacheDecorator;
-use App\Services\ProcessorInterface;
-use Illuminate\Contracts\Cache\Repository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,13 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(ProcessorInterface::class, Processor::class);
-        $this->app->extend(ProcessorInterface::class, function (ProcessorInterface $processor) {
-            return new ProcessorCacheDecorator(
-                app()->make(Repository::class),
-                $processor,
-            );
-        });
+        //
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-declare(strict_types=1);
-
-use App\Http\Controllers\ParseAction;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Psr\Log\LoggerInterface;
 
-Route::get('/', function (LoggerInterface $logger, Request $request) {
-    $logger->info('homepage opened', ['ip' => $request->ip()]);
-
-    // TODO render openAPI spec
-    return 'bestchange-parser';
+Route::get('/', function () {
+    return view('welcome');
 });
